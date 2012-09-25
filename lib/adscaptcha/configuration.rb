1878beebfd12c8d00d3382cccc81f683e2fd09aa
Jimmy Cuervo <richard.gonzales@gmail.com>
2012-09-26 00:26:30 +0800

    some cleanup

--- lib/adscaptcha/configuration.rb
+++ lib/adscaptcha/configuration.rb
@@ -4,30 +4,13 @@ module Adscaptcha
                   :public_key,
                   :private_key,
                   :ads_captcha_api
-    # attr_accessor :nonssl_api_server_url,
-    #               :ssl_api_server_url,
-    #               :verify_url,
-    #               :skip_verify_env,
-    #               :private_key,
-    #               :public_key,
-    #               :proxy,
-    #               :handle_timeouts_gracefully
 
-    def initialize #:nodoc:
-      # @nonssl_api_server_url      = RECAPTCHA_API_SERVER_URL
-      # @ssl_api_server_url         = RECAPTCHA_API_SECURE_SERVER_URL
-      # @verify_url                 = RECAPTCHA_VERIFY_URL
-      # @skip_verify_env            = SKIP_VERIFY_ENV
-      # @handle_timeouts_gracefully = HANDLE_TIMEOUTS_GRACEFULLY
+    def initialize
       @ads_captcha_api       = ADSCAPTCHA_API
       
       @private_key           = ENV['ADSCAPTCHA_PRIVATE_KEY']
       @public_key            = ENV['ADSCAPTCHA_PUBLIC_KEY']
       @captcha_id            = ENV['ADSCAPTCHA_ID']
     end
-
-    # def api_server_url(ssl = false) #:nodoc:
-    #   ssl ? ssl_api_server_url : nonssl_api_server_url
-    # end
   end
 end
